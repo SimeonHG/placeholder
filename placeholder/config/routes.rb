0adfb89cda2e02faf9895c6aa71451188dcb0a7a
Elliiie <eli.georgieva.06@gmail.com>
2019-03-08 17:52:42 +0200

    done

--- placeholder/config/routes.rb
+++ placeholder/config/routes.rb
@@ -3,10 +3,6 @@ Rails.application.routes.draw do
   resources :projects do
   	resources :tasks
   end
-  
-
-
+  get '/', to: 'main_page#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-
 end
