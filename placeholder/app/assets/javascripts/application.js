d3b27ac196b7fce4ae3ae3235a39cbaf9f2cd111
Elliiie <eli.georgieva.06@gmail.com>
2019-06-11 17:41:59 +0300

    added working drag and drop

--- placeholder/app/assets/javascripts/application.js
+++ placeholder/app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery2
 //= require popper
 //= require bootstrap
 //= require rails-ujs
 //= require turbolinks
+//= require dragula
